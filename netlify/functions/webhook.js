24c220315bdf0e1dedb0ecec5fba25bcd2db1098
Montaser Ali <montaserali4work@gmail.com>
2026-04-24 17:32:18 +0200

    Update webhook.js

--- netlify/functions/webhook.js
+++ netlify/functions/webhook.js
@@ -50,7 +50,7 @@ async function getGeminiReply(userMessage) {
   try {
     // استخدم الرابط الذي جلبته من قوقل ستوديو لأنه الأكثر استقراراً
     const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest:generateContent?key=${GEMINI_API_KEY}`,
+      `https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key=${GEMINI_API_KEY}`,
       {
         method: "POST",
         headers: { "Content-Type": "application/json" },
